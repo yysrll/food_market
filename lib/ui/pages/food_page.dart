a68ee84aea4d48b715246ddfdcda62d807fee145
yysrll <yusril.uzumaki@gmail.com>
2021-10-13 16:27:51 +0800

    feat: customTabBar in food_page

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -6,6 +6,8 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPageState extends State<FoodPage> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -72,6 +74,41 @@ class _FoodPageState extends State<FoodPage> {
               ),
             ),
             //// LIST OF FOOD WITH TABS
+            Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  CustomTabBar(
+                    titles: ["New Taste", "Popular", "Recommended"],
+                    selectedIndex: selectedIndex,
+                    onTap: (index) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  Builder(builder: (_) {
+                    String body = (selectedIndex == 0)
+                        ? 'New Taste Body'
+                        : (selectedIndex == 1)
+                            ? 'Popular Body'
+                            : 'Recommeded Body';
+
+                    return Center(
+                      child: Text(
+                        body,
+                        style: blackFontStyle2,
+                      ),
+                    );
+                  }),
+                  SizedBox(
+                    height: 80,
+                  )
+                ],
+              ),
+            ),
           ],
         )
       ],
